0c13c5c1a4d5aa8cf2aa56dc8c5e61fa863e51a2
Roush,Kyle <Kyle.Roush@cerner.com>
2020-05-01 14:58:46 -0500

    Updates

---
+++ precache-manifest.da341abf94184afe189658a362ed9628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac31de8d13e29b6e3bd835e931e6416",
+    "url": "/bowl-game/index.html"
+  },
+  {
+    "revision": "d063a05bf9905a6ebfcd",
+    "url": "/bowl-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aa5d4b335ef826bebee6",
+    "url": "/bowl-game/static/js/2.f5121839.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/bowl-game/static/js/2.f5121839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d063a05bf9905a6ebfcd",
+    "url": "/bowl-game/static/js/main.711eca95.chunk.js"
+  },
+  {
+    "revision": "4feaf638f18abb4220cc",
+    "url": "/bowl-game/static/js/runtime-main.5a3e2bd1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bowl-game/precache-manifest.da341abf94184afe189658a362ed9628.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bowl-game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
